5d4bdaed9074ab126f1e433ce81dd3a84e6cff50
w4nn4b3cod3r <kmjiaulislam@gmail.com>
2021-01-04 16:22:50 +0600

    completed todo app

--- todolist/forms.py
+++ todolist/forms.py
@@ -8,3 +8,22 @@ class TaskForm(forms.ModelForm):
         model = Task
         fields = "__all__"
         
+    title = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                # "type": "text",
+                "class": "form-control form-control-lg rounded-0",
+                "placeholder": "Enter task...",
+            }
+        ),
+    )
+
+    completed = forms.CharField(
+        required = False,
+        widget= forms.widgets.CheckboxInput(
+            attrs={
+                "type": "checkbox",
+                "class": "from-check-input",
+            }
+        ),
+    )
